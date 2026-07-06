8303dd4293922f7c09c86f4224f2efa44d71d059
Melissa Garrido <melissagarridosierra@gmail.com>
2026-07-06 07:30:06 -0500

    Add suppliers to Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -8,3 +8,11 @@ VALUES
 
 SELECT * FROM product
 
+INSERT INTO Suppliers (SupplierID, SupplierName, SupplierCity)
+VALUES
+(1, 'Aceros del Norte S.A.S', 'Cartagena'),
+(2, 'Aceros Norte', 'Cartagena'),
+(3, 'Industriales S.A.S', 'Barranquilla'),
+(4, 'Suministros Global SAS', 'Santa Marta');
+
+SELECT * FROM Suppliers
