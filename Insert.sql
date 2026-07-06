edddeff65a1a97341afd03ffdd853cfd22785914
Melissa Garrido <melissagarridosierra@gmail.com>
2026-07-06 07:21:07 -0500

    Add initial product entries to Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO Product (ProductID, ProductName, Category)
+VALUES
+(1, 'Casco Industrial', 'EPP'),
+(2, 'Disco de Corte 4.5', 'Herramienta'),
+(3, 'Electrodo E6013', 'Consumible'),
+(4, 'Guantes de Nitrilo', 'EPP'),
+(5, 'Soldadura E6013', 'Consumible');
+
+SELECT * FROM product
+
